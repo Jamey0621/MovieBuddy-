1f2bd18367fb288058c483f725618b1fb5df58a5
Jamey0621 <you@example.com>
2020-12-16 15:30:38 -0600

    Running and working

---
+++ script.js
@@ -0,0 +1,95 @@
+const APIURL =
+"https://api.themoviedb.org/3/discover/movie?primary_release_date.gte=2020-01-01&primary_release_date.lte=2020-12-16&api_key=00d191017b7abab337f6023b2a2aba4d";
+const IMGPATH = "https://image.tmdb.org/t/p/w1280";
+const SEARCHAPI =
+    "https://api.themoviedb.org/3/search/movie?&api_key=00d191017b7abab337f6023b2a2aba4d&query=";
+
+const main = document.getElementById("main");
+const form = document.getElementById("form");
+const search = document.getElementById("search");
+const OutNowUrl = "https://api.themoviedb.org/3/discover/movie?primary_release_date.gte=2014-09-15&primary_release_date.lte=2014-10-22&api_key=00d191017b7abab337f6023b2a2aba4d"
+
+// initially get fav movies
+getMovies(APIURL);
+
+async function getMovies(url) {
+    const resp = await fetch(url);
+    const respData = await resp.json();
+
+    console.log(respData);
+
+    showMovies(respData.results);
+
+    respData.results.forEach((movie) => {
+        const img = document.createElement("img");
+        img.src = IMGPATH + movie.poster_path;
+    });
+}
+
+//for movies out in theatures
+getMoviesOut(OutNowUrl);
+async function getMoviesOut(url) {
+    const resp = await fetch(url);
+    const respData = await resp.json();
+
+    showMoviesOut(respData.results)
+
+    respData.results.forEach((movie) =>{
+        const img = document.createElement("img");
+        img.src = IMGPATH + movie.poster_path;
+    });
+}
+
+function showMovies(movies) {
+    // clear main
+    main.innerHTML = "";
+
+    movies.forEach((movie) => {
+        const { poster_path, title, vote_average, overview } = movie;
+
+        const movieEl = document.createElement("div");
+        movieEl.classList.add("movie");
+
+        movieEl.innerHTML = `
+            <img
+                src="${IMGPATH + poster_path}"
+                alt="${title}"
+            />
+            <div class="movie-info">
+                <h3>${title}</h3>
+                <span class="${getClassByRate(
+                    vote_average
+                )}">${vote_average}</span>
+            </div>
+            <div class="overview">
+                <h3>Overview:</h3>
+                ${overview}
+            </div>
+        `;
+
+        main.appendChild(movieEl);
+    });
+}
+
+
+function getClassByRate(vote) {
+    if (vote >= 8) {
+        return "green";
+    } else if (vote >= 5) {
+        return "orange";
+    } else {
+        return "red";
+    }
+}
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const searchTerm = search.value;
+
+    if (searchTerm) {
+        getMovies(SEARCHAPI + searchTerm);
+
+        search.value = "";
+    }
+});
